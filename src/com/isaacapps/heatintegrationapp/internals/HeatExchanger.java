fed15ef1e11b43a5a474d82bcbd6c744aa0406ff
Isaac Lafrance <isaaclafrance@gmail.com>
2016-12-29 11:21:52 -0500

    Delete duplicate HeatExchanger.java
    A different version of this already exists in the energytransferelements folder.

--- src/com/isaacapps/heatintegrationapp/internals/HeatExchanger.java
+++
@@ -1,6 +0,0 @@
-package com.isaacapps.heatintegrationapp.internals;
-//TODO: Potentially Use a Genetic Algorithim Method to Find Network Solutions When Area and Cost Optimizations are Necessary
-
-public class HeatExchanger {
-
-}
